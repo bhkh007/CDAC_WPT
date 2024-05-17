1848da9a36b69d757f302cf083e8ea9de2e86994
bhkh007 <khandarbhavesh007@gmail.com>
2024-05-17 11:05:56 +0530

    commit these just created react app files

---
+++ login/src/App.js
@@ -0,0 +1,16 @@
+// App.js
+import React from 'react';
+import Login from '../src/components/Login';
+
+
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Login/>
+    </div>
+  );
+}
+
+export default App;
